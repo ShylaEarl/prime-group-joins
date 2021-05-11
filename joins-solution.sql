85f8ab2743ede0ae8969b8dd0eebc8e14b374abf
Shyla Earl <shyla.earl@gmail.com>
2021-05-11 11:56:02 -0500

    Q1, Q6, Q7 completed

--- joins-solution.sql
+++ joins-solution.sql
@@ -4,6 +4,9 @@
 
 --TODO
 -- 1. Get all customers and their addresses.
+SELECT customers.first_name, customers.last_name, addresses.street, 
+addresses.city, addresses.state, addresses.zip FROM customers
+JOIN addresses ON customers.id = addresses.customer_id;
 
 -- 2. Get all orders and their line items (orders, quantity and product).
 
@@ -13,9 +16,13 @@
 
 -- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 
--- 6. How many customers do we have?
+-- 6. How many customers do we have? 
+4
+SELECT COUNT(customers.id) FROM customers;
 
 -- 7. How many products do we carry?
+7
+SELECT COUNT(products.id) FROM products;
 
 -- 8. What is the total available on-hand quantity of diet pepsi?
 
