57b5cf23ff21b7cf360ad8717452ba926412200a
Shyla Earl <shyla.earl@gmail.com>
2021-05-12 21:29:52 -0500

    Q2 completed

--- joins-solution.sql
+++ joins-solution.sql
@@ -9,10 +9,15 @@ addresses.city, addresses.state, addresses.zip FROM customers
 JOIN addresses ON customers.id = addresses.customer_id;
 
 -- 2. Get all orders and their line items (orders, quantity and product).
+SELECT orders.id, line_items.product_id, line_items.quantity, products.description
+FROM orders JOIN line_items ON orders.id = line_items.order_id
+JOIN products on line_items.product_id = products.id;
 
 -- 3. Which warehouses have cheetos?
+3
 
 -- 4. Which warehouses have diet pepsi?
+1, 3, 4
 
 -- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 
@@ -25,6 +30,7 @@ SELECT COUNT(customers.id) FROM customers;
 SELECT COUNT(products.id) FROM products;
 
 -- 8. What is the total available on-hand quantity of diet pepsi?
+92
 
 -- Stretch
 -- 9. How much was the total cost for each order?
